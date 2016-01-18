f811b93cecc2327e773028fc0be7238c971a49c6
Henry Schaumburger <per4mnce@gmail.com>
2016-01-17 19:45:02 -0500

    Checkpoint 2 completed

--- dist/scripts/controllers/MainCtrl.js
+++ dist/scripts/controllers/MainCtrl.js
@@ -2,64 +2,56 @@
     function MainCtrl($interval) {
         console.log('Starting MainCtrl');
 
-        var SESSION_TIME = 5;         //Work session default time - 1500 (25 minutes)
-        var SHORT_SESSION_TIME = 300; //Short break session default time - 300 (5 minutes)
-        var LONG_SESSION_TIME = 1800; //Long break session default time - 1800 (30 minutes)
-
-        var stop = undefined;
-        this.time = SESSION_TIME;
-        
+        // Initialize
+        var WORK_TIME = 5; //Work session default time - 1500 (25 minutes)
+        var SHORT_BREAK_TIME = 3; //Short break session default time - 300 (5 minutes)
+        var LONG_BREAK_TIME = 10; //Long break session default time - 1800 (30 minutes)
+        var timer;
         var self = this; // This allows the inner function to gain access to 'this'
 
-        this.workButtonName = 'Start';  //Set the name op the work button
-        
-    
-        
-        
-        // Starts or resets the timer based on button text
-        // Initiated by the user button click
-        //Private function
-        this.startResetTimer = function() {
-            if (self.workButtonName == 'Start' || self.workButtonName == 'Break' ){
-                startTimer();
+        this.onBreak = false;
+        this.time = WORK_TIME; // Initial timer setting
+        this.buttonName = 'Start'; //Set the name op the work button
+
+
+        // Public function.  Called by button in main.html template
+        this.startResetTimer = function () {
+            console.log('startResetTimer() started');
+            if (self.buttonName === 'Start') {
+                timer = $interval(countdown, 1000);
+            } else {
+                //alert("Reset Button");
+                setTimer();
             }
-            else{
-                stopTimer();
-            }  
         };
-        
-        //Reset
-        stopTimer = function() {
-            $interval.cancel(stop);
-            self.workButtonName = 'Start';
-            self.time = SESSION_TIME;
-        }
-        
-        // Start the countdown timer
-        // Private Function.
-        startTimer = function() {
-            console.log('self.startTimer:  ' + self.time);
-            self.workButtonName = 'Reset';
-                
-            stop = $interval(function() {
-                console.log('$interval function started:  ' + self.time);
-                if (self.time > 0 ) {
-                    console.log('self.time > 0');
-                    self.time -= 1;
-                }
-                else {
-                    $interval.cancel(stop);
-                    stop = undefined;
-                    console.log('$interval function stopped: ' + self.time)
-                }
-            }, 1000);
+
+        // Starts the countdown clock
+        var countdown = function () {
+            self.time -= 1;
+            self.buttonName = "Reset";
+            console.log("countdown " + self.time);
+
+            if (self.time <= 0) {
+                $interval.cancel(timer); //Stop Timer
+                //self.onBreak ? setWork() : setBreak();
+                self.onBreak = !self.onBreak;  //Change from current break type to the other
+                setTimer();
+            }
         };
-        
-        
-        
-    
-    } //End of MainCtrl
-    
+
+        var setTimer = function () {
+            $interval.cancel(timer); //Stop timer
+            self.buttonName = "Start";
+            if (self.onBreak) {
+                self.time = SHORT_BREAK_TIME;
+            } else {
+                self.time = WORK_TIME;
+            }
+        };
+
+    } // End of MainCtrl
+
+
     angular
         .module('blocTime')
         .controller('MainCtrl', ['$interval', MainCtrl]);
